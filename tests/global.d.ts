861e7fafe23186cad4e8b28f562010c652eb7080
Nora Söderlund <norasoderlund@icloud.com>
2023-05-21 20:36:31 +0200

    Added tests

--- tests/global.d.ts
+++ tests/global.d.ts
@@ -2,7 +2,7 @@ declare global {
     namespace NodeJS {
         interface ProcessEnv {
             VITEST_AVATAR_API: string;
-            VITEST_AVATAR_TOKEN: string;
+            VITEST_SERVICE_TOKEN: string;
         }
     }
 }
--- tests/smoke.test.ts
+++ tests/smoke.test.ts
@@ -2,8 +2,7 @@ import { describe, expect, test } from "vitest";
 import Client, * as tests from "../src/index";
 
 describe("Smoke tests", () => {
-    const client = new Client(process.env.VITEST_AVATAR_API, process.env.VITEST_AVATAR_TOKEN);
-
+    const client = new Client(process.env.VITEST_AVATAR_API, process.env.VITEST_SERVICE_TOKEN);
 
     test("ping", async () => {
         const response = await tests.ping(client);
