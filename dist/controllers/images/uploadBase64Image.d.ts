160a093dc83bed452a9a2859db9629a49d236e27
Nora Söderlund <norasoderlund@icloud.com>
2023-05-23 18:40:10 +0200

    Added uploadBase64Image wrapper

---
+++ dist/controllers/images/uploadBase64Image.d.ts
@@ -0,0 +1,3 @@
+import { UploadImageResponse } from "./uploadImage";
+export default function uploadBase64Image(url: string, name: string, type: string, base64: string): Promise<UploadImageResponse>;
+//# sourceMappingURL=uploadBase64Image.d.ts.map
---
+++ dist/controllers/images/uploadBase64Image.js
@@ -0,0 +1,22 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const uploadImage_1 = __importDefault(require("./uploadImage"));
+function uploadBase64Image(url, name, type, base64) {
+    return __awaiter(this, void 0, void 0, function* () {
+        return (0, uploadImage_1.default)(url, name, type, Buffer.from(base64.split(';base64,')[1], 'base64'));
+    });
+}
+exports.default = uploadBase64Image;
+;
